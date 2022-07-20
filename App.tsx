755dffd9f8da4c6afb7b5de612dd48cdf9be2e17
Douglas <dou_glas_grando@hotmail.com>
2022-07-19 22:44:32 -0300

    add routes and navigation

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/
 
 import { THEME } from './src/styles/theme';
 
-import { SignIn } from './src/screens/SignIn';
+import { Routes } from './src/routes';
 
 import { Loading } from './src/components/Loading';
 
@@ -18,7 +18,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       /> 
-      { fontsLoaded ? <SignIn />  : <Loading />}
+      { fontsLoaded ? <Routes />  : <Loading />}
     </NativeBaseProvider>
   );
 }
